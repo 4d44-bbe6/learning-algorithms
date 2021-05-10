41ead2219e7ce9c68428b493269657ec5cb4b91a
Jeroen van der Schaaf <j.vanderschaaf@cmcgroep.nl>
2021-05-10 17:13:02 +0200

    functions back to <code> blocks

--- src/components/Sorting/MergeSort/index.js
+++ src/components/Sorting/MergeSort/index.js
@@ -44,7 +44,18 @@ mergeSort(values, 0, values.length)}
       </p>
       <pre>
         {`
-export const mergeSort = ${mergeSort}
+export const mergeSort = async (
+  array,
+  startIndex = 0,
+  endIndex = array.length - 1
+) => {
+  if (endIndex - startIndex > 1) {
+    let middleIndex = startIndex + ((endIndex - startIndex) >> 1);
+    await mergeSort(array, startIndex, middleIndex);
+    await mergeSort(array, middleIndex, endIndex);
+    await merge(array, startIndex, middleIndex, endIndex);
+  }
+};
         `}
       </pre>
       <p className="py-2">
@@ -60,7 +71,34 @@ export const mergeSort = ${mergeSort}
         compare them to, we add them back to the array.
       </p>
       <pre>{`
-const merge = ${merge}
+      export const merge = async (array, startIndex, middleIndex, EndIndex) => {
+  let tmp = []; // temp array for storing elements
+  let length = middleIndex - startIndex;
+
+  // Copy elements from the selection of the main array to temp array
+  for (let i = 0; i < length; i++) {
+    tmp[i] = array[startIndex + i];
+  }
+
+  let j = 0;
+  while (j < length && middleIndex < EndIndex) {
+    if (tmp[j] <= array[middleIndex]) {
+      // Wait one ms for visualization
+      await sleep(1);
+      array[startIndex++] = tmp[j++];
+    } else {
+      // Wait one ms for visualization
+      await sleep(1);
+      array[startIndex++] = array[middleIndex++];
+    }
+  }
+
+  while (j < length) {
+    // Wait one ms for visualizations
+    await sleep(1);
+    array[startIndex++] = tmp[j++];
+  }
+};
         `}</pre>
       <div>
         <span className="font-bold">Big-O Analysis</span>
